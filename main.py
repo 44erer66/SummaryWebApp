8a8b8a27ec0e61d23056485d665e920756575195
44erer66 <dhrunarunneer@gmail.com>
2020-07-06 15:30:53 -0700

    The basic files for our project

---
+++ main.py
@@ -0,0 +1,13 @@
+from summarization import Summarization
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hi"
+
+
+while(1):
+    if __name__ == "__main__":
+        app.run()
---
+++ summarization.py
@@ -0,0 +1,3 @@
+class Summarization():
+    def __init__(self):
+        pass
